4016f96d0659d3d2407b215999caf47e1919beae
Alpha <43486986+sudoAlphaX@users.noreply.github.com>
2021-02-12 22:03:57 +0530

    improve error handlers

--- handlers/error.py
+++ handlers/error.py
@@ -1,32 +1,28 @@
+from telegram import InlineKeyboardMarkup, InlineKeyboardButton
 from secrets import LOG_CHAT
 
+
 def error(update, context):
-
-    context.bot.send_message(chat_id = LOG_CHAT, text = ("""
-
-#{username}
-
-User ID: {userid}
-Chat ID: {chatid}
-
-Error:
-{error}
-
-Update:
-{update}
-
-""").format(username=context.bot.username, userid = update.message.from_user.id if update.message.from_user else '', chatid = update.message.chat.id if update.message.chat else '', error=context.error, update = update))
 
     try:
         if context.error.message == "Have no rights to send a message":
             update.message.chat.leave()
+            return
+
+        elif context.error.message == "Conflict: terminated by other getUpdates request; make sure that only one bot instance is running":
+            return
 
         else:
-            update.message.reply_text("⚠ An unexpected error occured, the error report was forwarded to the developers.", reply_markup = ([[delete_button]]))  
+            update.message.reply_text("⚠ An unexpected error occured, the error report was forwarded to the developers.", reply_markup=(
+                [[InlineKeyboardButton("OK", callback_data=f"delete_{update.message.from_user.id}")]]))
     except:
         pass
 
+    context.bot.send_message(
+        LOG_CHAT, f"#{context.bot.username}\n\n\nError:\n{context.error}\n\nUpdate:\n{update}"
+    )
+
 
 __handlers__ = [
-   ["error",error]
+    ["error", error]
 ]
