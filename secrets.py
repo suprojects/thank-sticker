81ee2964816d4188c7befb8a55d22ee938096461
Alpha <43486986+sudoAlphaX@users.noreply.github.com>
2021-02-12 21:46:59 +0530

    add DB_NAME

--- secrets.py
+++ secrets.py
@@ -9,3 +9,5 @@ import os
 BOT_TOKEN = os.environ.get("BOT_TOKEN")
 SUDO_USERS = list(map(int, os.environ.get("SUDO_USERS").split()))
 LOG_CHAT = int(os.environ.get('LOG_CHAT'))
+URI = os.environ.get("URI")
+DB_NAME = os.environ.get("DB_NAME")
