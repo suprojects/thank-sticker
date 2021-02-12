df1c2ef45da9e60cd6b112dd54a44ff6bcae9417
Alpha <43486986+sudoAlphaX@users.noreply.github.com>
2021-02-12 15:48:30 +0000

    config.py support

--- secrets.py
+++ secrets.py
@@ -1,14 +1,11 @@
-import os
+try:
+    from config import *
 
-if os.path.exists('tokens.py'):
+except ImportError:
+    pass
 
-    from tokens import *
-    
-    os.environ['BOT_TOKEN'] = BOT_TOKEN
-    os.environ['SUDO_USERS'] = '1178472788'
-    os.environ['LOG_CHAT'] = LOG_CHAT
-
+import os   
 
 BOT_TOKEN = os.environ.get("BOT_TOKEN")
-SUDO_USERS = int(os.environ.get("SUDO_USERS"))
+SUDO_USERS = list(map(int, environ.get("SUDO_USERS").split()))
 LOG_CHAT = int(os.environ.get('LOG_CHAT'))
